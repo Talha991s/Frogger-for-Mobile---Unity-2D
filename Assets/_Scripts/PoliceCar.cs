d5d0cbade63ab1e316153845535b974c9a8fe5bd
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-10-22 14:54:34 -0700

    Implementing police

---
+++ Assets/_Scripts/PoliceCar.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Filename: PoliceCar.cs
+ Author: Salick Talhah
+ Student Number: 101214166
+ Date last modified: 15/10/2020
+ Description: This file determine the movement of the Police car in the scene, things such as 
+              the speed, direction.
+ Revision History:
+ 28/09/2020
+ 29/09/2020
+ 15/10/2020
+ 22/10/2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceCar : MonoBehaviour
+{
+    public Rigidbody2D rb2;
+
+    [SerializeField]
+    float speed = 1.0f;
+
+     float minspeed = 3.0f;
+     float maxspeed = 5.0f;
+
+    void Start()
+    {
+        // the speed of the car will be randomized in this range
+        speed = Random.Range(minspeed, maxspeed);
+    }
+
+    // use fix update since we are dealing with AI and physics
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb2.MovePosition(rb2.position + forward * Time.fixedDeltaTime * speed);
+    }
+
+
+}
